11579e5c4d2b810f51753395796715a76dfab787
D313m <pinko.ikaro@gmail.com>
2025-05-18 01:00:00 +0200

    Fixed styling

--- sources/moving_average_filter.vhd
+++ sources/moving_average_filter.vhd
@@ -11,12 +11,12 @@ entity moving_average_filter is
 	Port (
 		aclk          : in  std_logic;
 		aresetn       : in  std_logic;
-
+		
 		s_axis_tvalid : in  std_logic;
 		s_axis_tdata  : in  std_logic_vector(TDATA_WIDTH - 1 downto 0);
 		s_axis_tlast  : in  std_logic;
 		s_axis_tready : out std_logic;
-
+		
 		m_axis_tvalid : out std_logic;
 		m_axis_tdata  : out std_logic_vector(TDATA_WIDTH - 1 downto 0);
 		m_axis_tlast  : out std_logic;
