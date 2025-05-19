82eacf0206546ee124298bf3080bfa5b6196aeb2
smolpatches <smolpatches>
2025-05-19 14:29:37 +0200

    FSM for jstk2 control

--- sources/digilent_jstk2.vhd
+++ sources/digilent_jstk2.vhd
@@ -36,14 +36,95 @@ end digilent_jstk2;
 
 architecture Behavioral of digilent_jstk2 is
 
-	-- Code for the SetLEDRGB command, see the JSTK2 datasheet.
-	constant CMDSETLEDRGB		: std_logic_vector(7 downto 0) := x"84";
+    -- FSM state encoding
+    constant STATE_IDLE : integer := 0;
+    constant STATE_LOAD : integer := 1;
+    constant STATE_SEND : integer := 2;
+    constant STATE_DONE : integer := 3;
 
-	-- Do not forget that you MUST wait a bit between two packets. See the JSTK2 datasheet (and the SPI IP-Core README).
-	------------------------------------------------------------
+    signal state      : integer range 0 to 3 := STATE_IDLE;
+
+    signal tx_buffer  : std_logic_vector(31 downto 0); -- 4 bytes to send
+    signal tx_index   : integer range 0 to 3 := 0;
+
+    signal rx_buffer  : std_logic_vector(31 downto 0); -- 4 bytes received
+    signal rx_index   : integer range 0 to 3 := 0;
+
+    signal delay_cnt  : integer range 0 to CLKFREQ / 1_000_000 * DELAY_US := 0;
 
 begin
 
-	-- ...
+    process(aclk)
+    begin
+        if rising_edge(aclk) then
+            if aresetn = '0' then
+                state <= STATE_IDLE;
+                m_axis_tvalid <= '0';
+                tx_index <= 0;
+                rx_index <= 0;
+                delay_cnt <= 0;
+            else
+                case state is
 
-end architecture;
+                    when STATE_IDLE =>
+                        -- Prepare LED command packet
+                        tx_buffer(31 downto 24) <= x"84";    -- CMDSETLEDRGB
+                        tx_buffer(23 downto 16) <= led_r;
+                        tx_buffer(15 downto 8)  <= led_g;
+                        tx_buffer(7 downto 0)   <= led_b;
+                        tx_index <= 0;
+                        rx_index <= 0;
+                        state <= STATE_LOAD;
+
+                    when STATE_LOAD =>
+                        if m_axis_tready = '1' then
+                            m_axis_tvalid <= '1';
+                            m_axis_tdata <= tx_buffer(31 - tx_index*8 downto 24 - tx_index*8);
+                            state <= STATE_SEND;
+                        end if;
+
+                    when STATE_SEND =>
+                        if m_axis_tready = '1' then
+                            -- Receive data in parallel
+                            if s_axis_tvalid = '1' then
+                                rx_buffer(31 - rx_index*8 downto 24 - rx_index*8) <= s_axis_tdata;
+                                rx_index <= rx_index + 1;
+                            end if;
+
+                            tx_index <= tx_index + 1;
+
+                            if tx_index = 3 then
+                                m_axis_tvalid <= '0';
+                                state <= STATE_DONE;
+                                delay_cnt <= 0;
+                            else
+                                m_axis_tdata <= tx_buffer(31 - (tx_index+1)*8 downto 24 - (tx_index+1)*8);
+                            end if;
+                        end if;
+
+                    when STATE_DONE =>
+                        jstk_x <= rx_buffer(31 downto 24) & rx_buffer(15 downto 14); -- 8 bits + 2 bits = 10 bits
+                        jstk_y <= rx_buffer(23 downto 16) & rx_buffer(13 downto 12); -- 8 bits + 2 bits = 10 bits
+                        btn_trigger <= rx_buffer(9);
+                        btn_jstk    <= rx_buffer(8);
+                        if delay_cnt < CLKFREQ / 1_000_000 * DELAY_US then
+                            delay_cnt <= delay_cnt + 1;
+                        else
+                            state <= STATE_IDLE;
+                        end if;
+
+                    when others =>
+                        state <= STATE_IDLE;
+
+                end case;
+            end if;
+        end if;
+    end process;
+
+    -- Joystick output parsing (example based on received 4 bytes)
+    -- jstk_x <= rx_buffer(31 downto 22);  Top 10 bits of first byte pair
+    -- jstk_y <= rx_buffer(21 downto 12);  Next 10 bits
+    -- btn_jstk <= rx_buffer(11);         Example button
+    -- btn_trigger <= '0';                Not handled yet
+
+end Behavioral;
