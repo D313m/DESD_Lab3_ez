7d370e6716f59ac250b6bfe63f29d25f8e85ef60
D313m <pinko.ikaro@gmail.com>
2025-05-30 18:25:37 +0200

    Removed comments left by the professors. Updated style

--- sources/digilent_jstk2.vhd
+++ sources/digilent_jstk2.vhd
@@ -3,47 +3,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity digilent_jstk2 is
 	generic (
-		DELAY_US		: integer := 25;    -- Delay (in us) between two packets
-		CLKFREQ		 	: integer := 100_000_000;  -- Frequency of the aclk signal (in Hz)
-		SPI_SCLKFREQ 	: integer := 66_666 -- Frequency of the SPI SCLK clock signal (in Hz)
+		DELAY_US      : integer := 25;           -- Delay [us] between two packets
+		CLKFREQ       : integer := 100_000_000;  -- Frequency of the aclk signal [Hz]
+		SPI_SCLKFREQ  : integer := 50_000        -- Frequency of the SPI SCLK clock signal [Hz]
 	);
 	Port ( 
-		aclk 			: in  STD_LOGIC;
-		aresetn			: in  STD_LOGIC;
-
-		-- Data going TO the SPI IP-Core (and so, to the JSTK2 module)
-		m_axis_tvalid	: out STD_LOGIC;
-		m_axis_tdata	: out STD_LOGIC_VECTOR(7 downto 0);
-		m_axis_tready	: in STD_LOGIC;
-
-		-- Data coming FROM the SPI IP-Core (and so, from the JSTK2 module)
-		-- There is no tready signal, so you must be always ready to accept and use the incoming data, or it will be lost!
-		s_axis_tvalid	: in STD_LOGIC;
-		s_axis_tdata	: in STD_LOGIC_VECTOR(7 downto 0);
-
+		aclk          : in  std_logic;
+		aresetn	      : in  std_logic;
+		
+		-- Data going to the SPI IP-Core
+		m_axis_tvalid : out std_logic;
+		m_axis_tdata  : out std_logic_vector(7 downto 0);
+		m_axis_tready : in  std_logic;
+		
+		-- Data coming from the SPI IP-Core
+		s_axis_tvalid : in  std_logic;
+		s_axis_tdata  : in  std_logic_vector(7 downto 0);
+		
 		-- Joystick and button values read from the module
-		jstk_x			: out std_logic_vector(9 downto 0);
-		jstk_y			: out std_logic_vector(9 downto 0);
-		btn_jstk		: out std_logic;
-		btn_trigger		: out std_logic;
-
-		-- LED color to send to the module
-		led_r			: in std_logic_vector(7 downto 0);
-		led_g			: in std_logic_vector(7 downto 0);
-		led_b			: in std_logic_vector(7 downto 0)
+		jstk_x        : out std_logic_vector(9 downto 0);
+		jstk_y        : out std_logic_vector(9 downto 0);
+		btn_jstk      : out std_logic;
+		btn_trigger   : out std_logic;
+		
+		-- LED RGB values to send to the module
+		led_r         : in  std_logic_vector(7 downto 0);
+		led_g         : in  std_logic_vector(7 downto 0);
+		led_b         : in  std_logic_vector(7 downto 0)
 	);
 end digilent_jstk2;
 
 architecture Behavioral of digilent_jstk2 is
-
-	-- Code for the SetLEDRGB command, see the JSTK2 datasheet.
-	constant CMDSETLEDRGB		: std_logic_vector(7 downto 0) := x"84";
-
-	-- Do not forget that you MUST wait a bit between two packets. See the JSTK2 datasheet (and the SPI IP-Core README).
-	------------------------------------------------------------
-
+	-- Code for the SetLEDRGB command, as per the JSTK2 datasheet.
+	constant CMDSETLEDRGB : std_logic_vector(7 downto 0) := x"84";
+	
+	
 begin
-
-	-- ...
-
+	
+	
+	
 end architecture;
