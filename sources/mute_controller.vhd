b58d015710937cf6b6590f4a5994058eb0dd67e1
D313m <pinko.ikaro@gmail.com>
2025-05-30 21:33:11 +0200

    Updated style

--- sources/mute_controller.vhd
+++ sources/mute_controller.vhd
@@ -4,23 +4,23 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity mute_controller is
 	Generic (
-		TDATA_WIDTH		: positive := 24
+		TDATA_WIDTH   : positive := 24
 	);
 	Port (
-		aclk			: in std_logic;
-		aresetn			: in std_logic;
-
-		s_axis_tvalid	: in std_logic;
-		s_axis_tdata	: in std_logic_vector(TDATA_WIDTH-1 downto 0);
-		s_axis_tlast	: in std_logic;
-		s_axis_tready	: out std_logic;
-
-		m_axis_tvalid	: out std_logic;
-		m_axis_tdata	: out std_logic_vector(TDATA_WIDTH-1 downto 0);
-		m_axis_tlast	: out std_logic;
-		m_axis_tready	: in std_logic;
-
-		mute			: in std_logic
+		aclk          : in  std_logic;
+		aresetn       : in  std_logic;
+		
+		s_axis_tvalid : in  std_logic;
+		s_axis_tdata  : in  std_logic_vector(TDATA_WIDTH-1 downto 0);
+		s_axis_tlast  : in  std_logic;
+		s_axis_tready : out std_logic;
+		
+		m_axis_tvalid : out std_logic;
+		m_axis_tdata  : out std_logic_vector(TDATA_WIDTH-1 downto 0);
+		m_axis_tlast  : out std_logic;
+		m_axis_tready : in  std_logic;
+		
+		mute          : in  std_logic
 	);
 end mute_controller;
 
@@ -32,8 +32,7 @@ signal status : STATUS_t := ALL_PASS_state;
 signal m_axis_tvalid_sig : std_logic;
 
 begin
-
-	process (aclk, aresetn)
+	process(aclk, aresetn)
 	begin
 		if aresetn = '0' then
 			status <= ALL_PASS_state;
@@ -51,7 +50,7 @@ begin
 					
 				when MUTE_state =>
 					if mute = '0' and (m_axis_tready = '1' or m_axis_tvalid_sig = '0') then
-							status <= ALL_PASS_state;
+						status <= ALL_PASS_state;
 					else
 						status <= MUTE_state;
 					end if;
@@ -62,12 +61,11 @@ begin
 		end if;
 	end process;
 	
+	m_axis_tvalid_sig <= s_axis_tvalid;
 	
-	m_axis_tvalid 		<= m_axis_tvalid_sig;
+	m_axis_tvalid     <= m_axis_tvalid_sig;
+	m_axis_tdata      <= s_axis_tdata when status = ALL_PASS_state else (Others => '0');
+	s_axis_tready     <= m_axis_tready;
+	m_axis_tlast      <= s_axis_tlast;
 	
-	m_axis_tvalid_sig 	<= s_axis_tvalid;
-	m_axis_tdata 		<= s_axis_tdata when status = ALL_PASS_state else (Others => '0');
-	s_axis_tready 		<= m_axis_tready;
-	m_axis_tlast 		<= s_axis_tlast;
-
 end Behavioral;
