cabafdec15a84b4ce219a5360adc162bd35c5ea9
Giulio Faure Ragani <giulio.faureragani@gmail.com>
2025-06-01 20:41:57 +0200

    Fixed timing in balance_controller.vhd by making efficient division by 2

--- sources/balance_controller.vhd
+++ sources/balance_controller.vhd
@@ -4,13 +4,13 @@ use ieee.numeric_std.all;
 
 entity balance_controller is
 	generic (
-		TDATA_WIDTH	: positive := 24;
+		TDATA_WIDTH		: positive := 24;
 		BALANCE_WIDTH	: positive := 10;
 		BALANCE_STEP_2	: positive := 6		-- i.e., balance_values_per_step = 2**VOLUME_STEP_2
 	);
 	Port (
-		aclk		: in std_logic;
-		aresetn		: in std_logic;
+		aclk			: in std_logic;
+		aresetn			: in std_logic;
 
 		s_axis_tvalid	: in std_logic;
 		s_axis_tdata	: in std_logic_vector(TDATA_WIDTH-1 downto 0);
@@ -22,7 +22,7 @@ entity balance_controller is
 		m_axis_tready	: in std_logic;
 		m_axis_tlast	: out std_logic;
 
-		balance		: in std_logic_vector(BALANCE_WIDTH-1 downto 0)
+		balance			: in std_logic_vector(BALANCE_WIDTH-1 downto 0)
 	);
 end balance_controller;
 
@@ -57,7 +57,7 @@ begin
 					
 					for i in 0 to L_data'HIGH-1 loop
 						if balance_sig_normalized > + ((2**BALANCE_STEP_2) * i + (2**(BALANCE_STEP_2-1))) then -- in our case balance_sig_normalized > + (64 * i + 32)
-							L_data(i+1) <= L_data(i)/2;
+							L_data(i+1) <= shift_right(L_data(i), 1); -- shift a destra aritmetico (divisione per 2 ottimizzata per signed)
 						else
 							L_data(i+1) <= L_data(i);
 						end if;
@@ -69,7 +69,7 @@ begin
 					
 					for i in 0 to R_data'HIGH-1 loop
 						if balance_sig_normalized < - ((2**BALANCE_STEP_2) * i + (2**(BALANCE_STEP_2-1))) then -- in our case balance_sig_normalized < - (64 * i + 32)
-							R_data(i+1) <= R_data(i)/2;
+							R_data(i+1) <= shift_right(R_data(i), 1); -- shift a destra aritmetico (divisione per 2 ottimizzata per signed)
 						else
 							R_data(i+1) <= R_data(i);
 						end if;
