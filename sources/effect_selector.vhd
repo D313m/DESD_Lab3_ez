db23f751148378aabe11c6ee33ea3efbd24089da
Giulio Faure Ragani <giulio.faureragani@gmail.com>
2025-06-01 21:38:47 +0200

    First version of effect_selector.vhd

--- sources/effect_selector.vhd
+++ sources/effect_selector.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/29/2024 10:12:03 AM
--- Design Name: 
--- Module Name: effect_selector - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity effect_selector is
     generic(
@@ -41,6 +11,7 @@ entity effect_selector is
         effect : in STD_LOGIC;
         jstck_x : in STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
         jstck_y : in STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
+		
         volume : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
         balance : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
         lfo_period : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0)
@@ -50,5 +21,24 @@ end effect_selector;
 architecture Behavioral of effect_selector is
 
 begin
+
+	process(aclk, aresetn)
+	begin
+		if aresetn ='0' then
+			volume <= (Others => '0');
+			balance <= (Others => '0');
+			lfo_period <= (Others => '1'); -- the longest period
+			
+		elsif rising_edge(aclk) then
+		
+			if effect = '1' then
+				lfo_period <= jstck_y;
+			else
+				volume <= jstck_y;
+				balance <= jstck_x;
+			end if;
+		
+		end if;
+	end process;
 
 end Behavioral;
