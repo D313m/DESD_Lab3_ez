98dbd7b79f6fbc87e4346a1f1ea5211f40debadc
D313m <pinko.ikaro@gmail.com>
2025-06-02 21:46:10 +0200

    Modified module to compute volume and balance centering in it, instead of doing it downstream (trasforms them to signed values).

--- sources/effect_selector.vhd
+++ sources/effect_selector.vhd
@@ -1,41 +1,60 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity effect_selector is
-    generic(
-        JOYSTICK_LENGHT  : integer := 10
-    );
-    Port (
-        aclk : in STD_LOGIC;
-        aresetn : in STD_LOGIC;
-        effect : in STD_LOGIC;
-        jstck_x : in STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
-        jstck_y : in STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
+	generic(
+		JOYSTICK_LENGHT : integer := 10
+	);
+	Port (
+		aclk       : in  std_logic;
+		aresetn    : in  std_logic;
 		
-        volume : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
-        balance : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0);
-        lfo_period : out STD_LOGIC_VECTOR(JOYSTICK_LENGHT-1 downto 0)
-    );
+		effect     : in  std_logic;
+		jstck_x    : in  std_logic_vector(JOYSTICK_LENGHT - 1 downto 0);
+		jstck_y    : in  std_logic_vector(JOYSTICK_LENGHT - 1 downto 0);
+		
+		volume     : out std_logic_vector(JOYSTICK_LENGHT - 1 downto 0);
+		balance    : out std_logic_vector(JOYSTICK_LENGHT - 1 downto 0);
+		lfo_period : out std_logic_vector(JOYSTICK_LENGHT - 1 downto 0)
+	);
 end effect_selector;
 
 architecture Behavioral of effect_selector is
-
+	signal select_sig      : std_logic;
+	signal extended_signed : signed(JOYSTICK_LENGHT downto 0); -- 1 bit more
 begin
-
+	
 	process(aclk, aresetn)
+		variable centered_signed : signed(JOYSTICK_LENGHT - 1 downto 0);
 	begin
-		if aresetn ='0' then
-			volume <= (Others => '0');
-			balance <= (Others => '0');
-			lfo_period <= (Others => '1'); -- the longest period
+		if aresetn = '0' then
+			
+			volume     <= (Others => '0');
+			balance    <= (Others => '0');
+			lfo_period <= (Others => '1'); -- The longest period
+			
+			extended_signed <= (Others => '0');
+			select_sig <= '0';
 			
 		elsif rising_edge(aclk) then
-		
-			if effect = '1' then
-				lfo_period <= jstck_y;
+			
+			select_sig <= not select_sig;
+			
+			if select_sig = '1' then
+				extended_signed <= signed('0' & jstck_x);
 			else
-				volume <= jstck_y;
-				balance <= jstck_x;
+				extended_signed <= signed('0' & jstck_y);
+			end if;
+			
+			centered_signed := resize(extended_signed - 2**(JOYSTICK_LENGHT - 1), JOYSTICK_LENGHT);
+		
+			if effect = '1' and select_sig = '1' then
+				lfo_period <= std_logic_vector(extended_signed(lfo_period'RANGE)); -- Original value
+			elsif select_sig = '1' then
+				volume     <= std_logic_vector(centered_signed);
+			elsif effect = '0' then
+				balance    <= std_logic_vector(centered_signed);
 			end if;
 		
 		end if;
